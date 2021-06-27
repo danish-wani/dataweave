724c488858e210ad2ae0f1f40ab41193a1431d97
danish-wani <danish.wani@trialx.com>
2021-06-27 13:18:26 +0530

    -URL namespace removed

--- dataweave/urls.py
+++ dataweave/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('products.rest.urls', namespace='products')),
+    path('api/v1/', include('products.rest.urls')),
 ]
